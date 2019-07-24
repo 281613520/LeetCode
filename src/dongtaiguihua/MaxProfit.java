a0fde9e6e3e8ba654a19f20870f984fe64aa37bb
281613520 <281613520@qq.com>
2019-07-24 23:23:09 +0800

    7.24.2

---
+++ src/dongtaiguihua/MaxProfit.java
@@ -0,0 +1,44 @@
+package dongtaiguihua;
+
+/**
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ *
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ *
+ * 注意你不能在买入股票前卖出股票。
+ *
+ * 示例 1:
+ *
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * 示例 2:
+ *
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ */
+public class MaxProfit {
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int min = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (min > price) {
+                min = price;
+            }
+            if (price - min > maxProfit) {
+                maxProfit = price - min;
+            }
+
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+            int[] nums = {7,1,5,3,6,4};
+            System.out.println(maxProfit(nums));
+
+    }
+}
