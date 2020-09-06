de16b38af8d7dfb79bb7a7757a5a3c4f6096cd89
281613520 <281613520@qq.com>
2020-09-06 23:13:31 +0800

    34

---
+++ src/erchashu/LevelOrderBottom.java
@@ -0,0 +1,38 @@
+package erchashu;
+
+import math.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LevelOrderBottom {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            List<TreeNode> list = new ArrayList<>();
+            List<Integer> list2 = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                list.add(queue.removeFirst());
+            }
+
+            for (TreeNode node : list){
+                if (node != null) {
+                    list2.add(node.val);
+                    queue.add(node.left);
+                    queue.add(node.right);
+                }
+            }
+            if (list2.size() != 0) {
+                res.add(list2);
+            }
+        }
+
+        Collections.reverse(res);
+        return res;
+    }
+}
