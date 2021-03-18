984b43b86a92f711af77fb4b8a6dd94e79f26c4c
ankh <281613520@qq.com>
2021-03-18 22:54:16 +0800

    update

--- src/linklist/ReverseBetween.java
+++ src/linklist/ReverseBetween.java
@@ -11,7 +11,30 @@ package linklist;
  * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
  * 输出: 1->4->3->2->5->NULL
  */
+
+/**
+ *
+ * 补充go语言解法
+ * func reverseBetween(head *ListNode, left, right int) *ListNode {
+ *     // 设置 dummyNode 是这一类问题的一般做法
+ *     dummyNode := &ListNode{Val: -1}
+ *     dummyNode.Next = head
+ *     pre := dummyNode
+ *     for i := 0; i < left-1; i++ {
+ *         pre = pre.Next
+ *     }
+ *     cur := pre.Next
+ *     for i := 0; i < right-left; i++ {
+ *         next := cur.Next
+ *         cur.Next = next.Next
+ *         next.Next = pre.Next
+ *         pre.Next = next
+ *     }
+ *     return dummyNode.Next
+ * }
+ */
 public class ReverseBetween {
+
     public ListNode reverseBetween(ListNode head, int m, int n) {
         int[] result = new int[n - m + 1];
         int i = 1 ;
