965a9f98b4cc95bd69c11bb94a6a10f9950630f0
281613520 <281613520@qq.com>
2022-10-17 21:56:27 +0800

    10.17打卡

--- src/daily/Solution.java
+++ src/daily/Solution.java
@@ -10,17 +10,17 @@ public class Solution {
         Map<Integer, Set<Integer>> graph = new HashMap<>();
 
         for (int[] dislike : dislikes) {
-            if (graph.containsKey(dislike[0])){
+            if (graph.containsKey(dislike[0])) {
                 graph.get(dislike[0]).add(dislike[1]);
-            }else {
-                graph.put(dislike[0],new HashSet<Integer>(){{
+            } else {
+                graph.put(dislike[0], new HashSet<Integer>() {{
                     add(dislike[1]);
                 }});
             }
-            if (graph.containsKey(dislike[1])){
+            if (graph.containsKey(dislike[1])) {
                 graph.get(dislike[1]).add(dislike[0]);
-            }else {
-                graph.put(dislike[1],new HashSet<Integer>(){{
+            } else {
+                graph.put(dislike[1], new HashSet<Integer>() {{
                     add(dislike[0]);
                 }});
             }
@@ -31,7 +31,7 @@ public class Solution {
         Set<Integer> set2 = new HashSet<>();
 
         for (int i = 1; i <= n; i++) {
-            if (!set1.contains(i) && !set2.contains(i)){
+            if (!set1.contains(i) && !set2.contains(i)) {
                 if (!dfs(i, set1, set2, graph, 1)) {
                     return false;
                 }
@@ -43,11 +43,11 @@ public class Solution {
     }
 
     private boolean dfs(int num, Set<Integer> set1, Set<Integer> set2, Map<Integer, Set<Integer>> graph, int status) {
-        Set<Integer> tmp = graph.getOrDefault(num,new HashSet<>());
+        Set<Integer> tmp = graph.getOrDefault(num, new HashSet<>());
 
         if (status == 1) {
             set1.add(num);
-        } else if (status == 2){
+        } else if (status == 2) {
             set2.add(num);
         }
 
@@ -56,8 +56,8 @@ public class Solution {
                 return false;
             }
 
-            if (!set1.contains(value) && !set2.contains(value)){
-                if (!dfs(value,set1,set2,graph,3 - status)){
+            if (!set1.contains(value) && !set2.contains(value)) {
+                if (!dfs(value, set1, set2, graph, 3 - status)) {
                     return false;
                 }
             }
@@ -65,8 +65,30 @@ public class Solution {
         return true;
     }
 
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        int n = fruits.length;
+        while (j < n) {
+            map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+            while (map.size() > 2) {
+                map.put(fruits[i], map.getOrDefault(fruits[i], 0) - 1);
+                if (map.get(fruits[i]) == 0) {
+                    map.remove(fruits[i]);
+                }
+                i++;
+            }
+            ans = Math.max(ans, j - i + 1);
+            j++;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.possibleBipartition(3,new int[][]{{1,2},{1,3},{2,3}});
+        //s.possibleBipartition(3, new int[][]{{1, 2}, {1, 3}, {2, 3}});
+        s.totalFruit(new int[]{3,3,3,1,2,1,1,2,3,3,4});
     }
 }
--- src/dongtaiguihua/Solution.java
+++ src/dongtaiguihua/Solution.java
@@ -46,6 +46,38 @@ public class Solution {
         return dp[n];
     }
 
+    public int climbStairs(int n) {
+     // dp[i] = dp[i-1]+ dp[i-2]
+        if (n == 0 || n == 1) return 1;
+        if (n ==2) return 2;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        if (n < 3) return 0;
+
+        // dp[i] = min(dp[i-1] + cost[i-1],dp[i-2] + cost[i-2])
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 0;
+        for (int i = 3 ; i <= n ; i++){
+            dp[i] = Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.tribonacci(4);
