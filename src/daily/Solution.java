c20a87e0440b646be99e6d4b054b636943f2c2a2
281613520 <281613520@qq.com>
2023-03-27 21:46:55 +0800

    3.27 solution2

--- src/daily/Solution.java
+++ src/daily/Solution.java
@@ -1124,6 +1124,39 @@ public class Solution {
         return res;
     }
 
+    public int countSubstrings2(String s, String t) {
+        int res = 0;
+
+        int m = s.length();
+        int n = t.length();
+        int[][] dpr = new int[m+1][n+1];
+        int[][] dpl = new int[m+1][n+1];
+
+        for (int i = 0 ; i < m ; i++){
+            for (int j = 0 ; j < n ;j++ ){
+                dpl[i+1][j+1] = s.charAt(i) == t.charAt(j)?(dpl[i][j]+1):0;
+            }
+        }
+
+        for (int i = m-1 ; i >= 0 ; i--){
+            for (int j = n-1 ; j >= 0  ;j-- ){
+                dpr[i][j] = s.charAt(i) == t.charAt(j)?(dpr[i+1][j+1]+1):0;
+            }
+        }
+
+
+        for (int i = 0 ; i < m ; i++){
+            for (int j = 0 ; j < n ; j++){
+                if (s.charAt(i) != t.charAt(j)){
+                    res += (dpl[i][j] + 1) *(dpr[i+1][j+1] + 1);
+                }
+            }
+        }
+
+
+        return res;
+    }
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
