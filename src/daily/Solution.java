fe0919d2594fc6f84c4fc73c8de8269aea9dad33
ankh <281613520@qq.com>
2023-10-29 10:45:32 +0800

    10.7

--- src/daily/Solution.java
+++ src/daily/Solution.java
@@ -2474,6 +2474,22 @@ public class Solution {
 
     }
 
+    static final int MOD_2 = 1000000007;
+    public int numRollsToTarget(int n, int k, int target) {
+        int[][] f = new int[n + 1][target + 1];
+        f[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= target; ++j) {
+                for (int x = 1; x <= k; ++x) {
+                    if (j - x >= 0) {
+                        f[i][j] = (f[i][j] + f[i - 1][j - x]) % MOD_2;
+                    }
+                }
+            }
+        }
+        return f[n][target];
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.maxProfit4(2, new int[]{2, 4, 1});
