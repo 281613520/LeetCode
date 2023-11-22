9dab6c5d41f963806f44c274dcb868372dddf606
ankh <281613520@qq.com>
2023-11-22 21:05:47 +0800

    11.22

--- src/daily/Solution.java
+++ src/daily/Solution.java
@@ -2934,6 +2934,38 @@ public class Solution {
 
     }
 
+
+    public int minPathCost(int[][] grid, int[][] moveCost) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0 ; i < n ; i++){
+            dp[0][i] = grid[0][i];
+        }
+
+        for (int i = 1 ; i < m ;i++){
+            Arrays.fill(dp[i],Integer.MAX_VALUE);
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 1 ; i < m ;i++){
+            for (int j = 0 ; j < n ; j++){
+                for (int k = 0 ; k < n ;k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][k] + moveCost[grid[i - 1][k]][j] + grid[i][j]);
+                }
+            }
+        }
+
+        for (int i = 0 ; i < n ; i++){
+            ans = Math.min(ans,dp[m-1][i]);
+        }
+
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         //solution.smallestMissingValueSubtree(new int[]{-1,0,1,0,3,3}, new int[]{5,4,6,2,1,3});
