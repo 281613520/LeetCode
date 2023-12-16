2b374e517011e1f57f6171863265739a3c9e7c33
ankh <281613520@qq.com>
2023-12-16 22:22:04 +0800

    12.7

--- src/daily/Solution.java
+++ src/daily/Solution.java
@@ -3331,6 +3331,18 @@ class Node {
 
 
 
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2 ; i <= n ;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         //solution.smallestMissingValueSubtree(new int[]{-1,0,1,0,3,3}, new int[]{5,4,6,2,1,3});
