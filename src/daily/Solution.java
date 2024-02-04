3a5a00ef72bf4f29c813e569d7e6fa6e6dbb75ea
ankh <281613520@qq.com>
2024-02-04 15:41:22 +0800

    1.14

--- src/daily/Solution.java
+++ src/daily/Solution.java
@@ -3526,4 +3526,30 @@ class Node {
     }
 
 
+    // dp
+    public int stoneGameVII2(int[] stones) {
+        // dp[i,j] = max(sum[j+1] - sum[i+1] - dp[i+1,j],sum[j]- sum[i] - dp[i,j-1])
+        int n = stones.length;
+        int[][] dp = new int[n][n];
+
+
+        int[] preSum = new int[n+1];
+        preSum[0] = 0;
+        for (int i = 1 ; i < preSum.length ; i++){
+            preSum[i] = preSum[i-1] + stones[i-1];
+        }
+
+        for (int i = n-2 ; i >= 0 ; i--){
+            for (int j = i+1 ; j < n ; j++){
+                dp[i][j] = Math.max(preSum[j+1] - preSum[i+1] - dp[i+1][j],preSum[j] - preSum[i] - dp[i][j-1]);
+            }
+        }
+
+
+        return dp[0][n-1];
+
+
+    }
+
+
 }
