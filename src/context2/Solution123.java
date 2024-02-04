4108911da20238e37f25c5f6c0abe32e77ea6d66
ankh <281613520@qq.com>
2024-02-04 17:22:45 +0800

    1.14

--- src/context2/Solution123.java
+++ src/context2/Solution123.java
@@ -1,6 +1,7 @@
 package context2;
 
 import context.Solution381;
+import context.week5.UndergroundSystem;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -28,15 +29,18 @@ public class Solution123 {
 
 
     public int numberOfPairs(int[][] points) {
-        Arrays.sort(points, (p, q) -> p[0] != q[0] ? p[0] - q[0] : q[1] - p[1]);
+        Arrays.sort(points, (o1, o2) -> o1[0] == o2[0]? o2[1] - o1[1]: o1[0] - o2[0]);
+
+        int n = points.length;
         int ans = 0;
-        for (int i = 0; i < points.length; i++) {
-            int y0 = points[i][1];
+
+        for (int i = 0 ; i < n ; i++){
+            int y = points[i][1];
             int maxY = Integer.MIN_VALUE;
-            for (int j = i + 1; j < points.length; j++) {
-                int y = points[j][1];
-                if (y <= y0 && y > maxY) {
-                    maxY = y;
+            for (int j = i+1 ; j < n ; j++){
+                int y1 = points[j][1];
+                if (maxY < y1 && y1 <= y ){
+                    maxY = y1;
                     ans++;
                 }
             }
