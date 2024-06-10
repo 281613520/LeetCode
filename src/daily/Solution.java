5b419e058a9bfb6e446c2c6099af0255c43bb663
ankh <281613520@qq.com>
2024-06-10 14:56:25 +0800

    4.9

--- src/daily/Solution.java
+++ src/daily/Solution.java
@@ -4798,7 +4798,27 @@ public class Solution {
     }
 
     public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n+2];
+        // dp[i][j] =  max(dp[i][k] + val[i] * val[k] * val[j] + dp[k][j],dp[i][j])
+        arr[0] = 1;
+        arr[1] = 1;
 
+        for (int i = 1 ; i <= n ; i++){
+            arr[i] = nums[i-1];
+        }
+
+        int[][] dp = new int[n+2][n+2];
+
+        for (int i = arr.length -1; i >= 0; i--){
+            for (int j = i+2 ; j <= n+1 ; j++){
+                for (int k = i+1 ; k < j ; k++){
+                    dp[i][j] = Math.max(dp[i][j], dp[i][k] + arr[i]*arr[k]* arr[j]+ dp[k][j]);
+                }
+            }
+        }
+
+        return dp[0][n+1];
     }
 
 
